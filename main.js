4c82f1848eddadd86b5efcd6ac476e859d40e527
anaesposito <anaclaraesposito@gmail.com>
2020-09-16 20:43:04 -0300

    hamburguesa con fixed

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuHamburguesa = document.querySelector(".boton-hamburguesa");
 const menuBody = document.querySelector("#container-expand");
+const body = document.querySelector("body");
 
 menuHamburguesa.onclick = () => {
   menuBody.classList.toggle("ocultar");
+  body.classList.toggle("overlay");
 };
