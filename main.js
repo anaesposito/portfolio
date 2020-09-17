f4748c526190fc8dc8285cbb1d2d2b7a6e4cda53
anaesposito <anaclaraesposito@gmail.com>
2020-09-16 21:42:23 -0300

    listo realmente el toggle del menu expand

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const menuHamburguesa = document.querySelector(".boton-hamburguesa");
 const menuBody = document.querySelector("#container-expand");
 const body = document.querySelector("body");
 const links = document.querySelectorAll(".lista-expand");
+const headerExpand = document.querySelector(".menu-expand");
 
 menuHamburguesa.onclick = () => {
   menuBody.classList.toggle("ocultar");
   body.classList.toggle("overlay");
+  headerExpand.classList.toggle("fixed");
 };
 
 for (let link of links) {
   link.onclick = () => {
-    menuBody.classList.add("ocultar");
+    menuBody.classList.toggle("ocultar");
   };
 }
