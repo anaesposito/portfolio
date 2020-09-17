cd1d237a08a79089407fdf0aefdddb05a8dd1ef2
anaesposito <anaclaraesposito@gmail.com>
2020-09-17 15:04:55 -0300

    agregue icono cerrar expand, todo perfecto en toggle

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const menuHamburguesa = document.querySelector(".boton-hamburguesa");
 const menuBody = document.querySelector("#container-expand");
 const body = document.querySelector("body");
+const cerrarExpand = document.querySelector(".cerrar-expand");
 const links = document.querySelectorAll(".lista-expand");
 const headerExpand = document.querySelector(".menu-expand");
+const presentacion = document.querySelector(".presentacion");
 
 menuHamburguesa.onclick = () => {
   menuBody.classList.toggle("ocultar");
-  body.classList.toggle("overlay");
-  headerExpand.classList.toggle("fixed");
 };
 
 for (let link of links) {
   link.onclick = () => {
-    menuBody.classList.toggle("ocultar");
+    menuHamburguesa.onclick();
   };
 }
+
+cerrarExpand.onclick = () => {
+  menuBody.classList.toggle("ocultar");
+};
