56e9dc509dd31a79d451f924688276760b2c7ac1
anaesposito <anaclaraesposito@gmail.com>
2020-09-16 21:29:36 -0300

    listo el  toggle con transform y translate

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuHamburguesa = document.querySelector(".boton-hamburguesa");
 const menuBody = document.querySelector("#container-expand");
 const body = document.querySelector("body");
+const links = document.querySelectorAll(".lista-expand");
 
 menuHamburguesa.onclick = () => {
   menuBody.classList.toggle("ocultar");
   body.classList.toggle("overlay");
 };
+
+for (let link of links) {
+  link.onclick = () => {
+    menuBody.classList.add("ocultar");
+  };
+}
